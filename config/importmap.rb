073decd3ed77c803a0e10760d1a99f439235923b
Marc Köhlbrugge <subscriptions@marckohlbrugge.com>
2026-01-17 12:44:51 +0700

    Tidy local-time imports and pin metadata
    Keeps formatting consistent around the local-time setup.

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
 pin "highlight.js", to: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/es/highlight.min.js"
-pin "local-time"
+pin "local-time" # @3.0.3
