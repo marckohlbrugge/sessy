c45000aff1d14daf0344458352cec30cc255de3c
Marc Köhlbrugge <subscriptions@marckohlbrugge.com>
2026-08-05 13:17:15 +0100

    Add sign out to the hosted app header.
    The session destroy action already existed; signed-in SaaS users needed a visible way to reach it.

--- saas/test/controllers/infos_test.rb
+++ saas/test/controllers/infos_test.rb
@@ -17,4 +17,17 @@ class Sessy::Saas::InfosTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "[data-saas-badge]"
   end
+
+  test "header shows sign out to a signed-in user" do
+    user = User.create!(email_address: "signout@example.com")
+    accounts(:instance).memberships.create!(user: user)
+    post session_path, params: { email_address: user.email_address }
+    post session_code_path, params: { code: MagicLink.last.code }
+
+    get root_path
+    assert_response :success
+    assert_select "form[action=?]", session_path do
+      assert_select "button", text: "Sign out"
+    end
+  end
 end
